da038b20258dffc4841d075613b7589ede41e660
Cerek Hillen <cerekh@gmail.com>
2023-07-09 11:27:12 -0700

    rewrite main a bit :)

--- main.c
+++ main.c
@@ -8,46 +8,47 @@
 #include "usb.h"
 
 void turn_on_leds() {
-  PORTD &= ~(1 << PD5);
   PORTB &= ~(1 << PB0);
+  PORTD &= ~(1 << PD5);
 }
 
 void turn_off_leds() {
-  PORTD |= (1 << PD5);
   PORTB |= (1 << PB0);
+  PORTD |= (1 << PD5);
 }
 
 int main(int argc, char** argv) {
-  usb_init();
+    usb_init();
 
-  DDRC |= ((1 << 7) | (1 << 6));
+    // Set LEDs to output.
+    DDRB |= (1 << PB0);
+    DDRD |= (1 << PD5);
 
-  while (!get_usb_config_status()) {
-    // LED Animation
-    PORTC &= ~(1 << 6);
-    PORTC |= (1 << 7);
-    _delay_ms(100);
-    PORTC &= ~(1 << 7);
-    PORTC |= (1 << 6);
-    _delay_ms(100);
-  }
-  PORTC &= ~((1 << 6) | (1 << 7));
+    // Set buttons to input.
+    DDRD &= (~(1 << PD0) & ~(1 << PD1) & ~(1 << PD4));
 
-  PORTD = 0; // push nothing out of port 0 to start with...
-  DDRD &= ~(1 << PD1); // set PD1 to be input
-  PORTD |= (1 << PD1) | (1 << PD0) | (1 << PD4); // set PD1 to be pull-up
-  while (true) {
-      int left_pressed = (PIND & (1 << PD1)) == 0;
-      int down_pressed = (PIND & (1 << PD0)) == 0;
-      int right_pressed = (PIND & (1 << PD4)) == 0;
+    while (!get_usb_config_status()) {
+	turn_off_leds();
+	_delay_ms(100);
+	turn_on_leds();
+	_delay_ms(100);
+    }
 
-      if (left_pressed || down_pressed || right_pressed) {
-	  turn_on_leds();
-	  for (int i = 0; i < 6; i++) {
-	      send_keypress(KEY_A, 0);
-	  }
-      } else {
-	  turn_off_leds();
-      }
-  }
+    PORTD = 0; // push nothing out of port 0 to start with...
+    DDRD &= ~(1 << PD1); // set PD1 to be input
+    PORTD |= (1 << PD1) | (1 << PD0) | (1 << PD4); // set PD1 to be pull-up
+    while (true) {
+	int left_pressed = (PIND & (1 << PD1)) == 0;
+	int down_pressed = (PIND & (1 << PD0)) == 0;
+	int right_pressed = (PIND & (1 << PD4)) == 0;
+
+	if (left_pressed || down_pressed || right_pressed) {
+	    turn_on_leds();
+	    for (int i = 0; i < 6; i++) {
+		send_keypress(KEY_A, 0);
+	    }
+	} else {
+	    turn_off_leds();
+	}
+    }
 }
