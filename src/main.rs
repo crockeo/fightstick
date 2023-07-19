579ff723d3a7c0a2080114ef7df29f36fda184f4
Cerek Hillen <cerekh@gmail.com>
2023-07-19 12:39:29 -0700

    get it all working :)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-#![no_std]
-#![no_main]
+#![feature(asm_experimental_arch)]
 #![feature(lang_items)]
+#![no_main]
+#![no_std]
 
 static mut PINB: *mut u8 = 0x23 as *mut u8;
 static mut DDRB: *mut u8 = 0x24 as *mut u8;
@@ -13,23 +14,47 @@ static mut PORTD: *mut u8 = 0x2B as *mut u8;
 #[no_mangle]
 fn main() -> ! {
     unsafe {
-	*DDRB |= 1 << 0;
-	*DDRD |= 1 << 5;
+	// set LEDs to out
+        *DDRB |= 1 << 0;
+        *DDRD |= 1 << 5;
+
+	// set buttons to in
+	*DDRD &= !(1 << 1 | 1 << 0 | 1 << 4);
+
+	// set buttons to high, so they can be pulled low
+	*PORTD |= 1 << 1 | 1 << 0 | 1 << 4;
     }
 
-    unsafe { helper_usb_init(); }
+    unsafe {
+        helper_usb_init();
+    }
     while !is_initialized() {
-	unsafe {
-	    *PORTB ^= 1 << 0;
-	    *PORTD ^= 1 << 5;
-	}
-	delay_ms(100);
+        unsafe {
+            *PORTB ^= 1 << 0;
+            *PORTD ^= 1 << 5;
+        }
+        delay_ms(100);
     }
 
+    unsafe {
+	*PORTB |= 1 << 0;
+	*PORTD |= 1 << 5;
+    }
+
+    let ports: &[(u8, u8)] = &[(1, 0x04), (0, 0x05), (4, 0x06)];
     loop {
-	unsafe {
-	    usb_send();
+	for (i, (offset, scancode)) in ports.iter().enumerate() {
+	    unsafe {
+		if *PIND & (1 << offset) == 0 {
+		    keyboard_pressed_keys[i] = *scancode;
+		} else {
+		    keyboard_pressed_keys[i] = 0;
+		}
+	    }
 	}
+        unsafe {
+            usb_send();
+        }
     }
 }
 
@@ -40,9 +65,11 @@ fn is_initialized() -> bool {
 fn delay_ms(ms: u8) {
     const clock_speed_ms: u32 = 16_000;
     for _ in 0..clock_speed_ms {
-	for _ in 0..ms {
-	    unsafe { core::arch::asm!("nop"); }
-	}
+        for _ in 0..ms {
+            unsafe {
+                core::arch::asm!("nop");
+            }
+        }
     }
 }
 
@@ -60,7 +87,7 @@ extern "C" {
     fn helper_usb_init();
     fn usb_send();
     static usb_state: UsbState;
-    static keyboard_pressed_keys: [u8; 6];
+    static mut keyboard_pressed_keys: [u8; 6];
 }
 
 #[panic_handler]
@@ -70,4 +97,4 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
 
 #[lang = "eh_personality"]
 #[no_mangle]
-pub unsafe extern "C" fn eh_personality() { }
+pub unsafe extern "C" fn eh_personality() {}
